db3defa731ae8da27d0643c51f6f4f86993b7008
Robson-vieira3424 <robsonestudos883@gmail.com>
2025-12-08 20:45:05 -0300

    fix: Corrigindo o caminho de cadastrar estacao de trabalho

--- src/main/java/com/StockManager/Controller/EstacionWorkController.java
+++ src/main/java/com/StockManager/Controller/EstacionWorkController.java
@@ -7,9 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "*")
 @RestController()
-@RequestMapping("/estacion")
+@RequestMapping("/estacao")
 public class EstacionWorkController {
 
     @Autowired
